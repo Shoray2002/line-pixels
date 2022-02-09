a65ddbcbbcb1a226b497beee23c833a0022f8c55
Shoray Singhal <lci2020037@iiitl.ac.in>
2022-02-09 14:22:10 +0530

    adding logic for top right corner

--- src/lineMP.mjs
+++ src/lineMP.mjs
@@ -67,13 +67,19 @@ function straightLine(plane1, plane2) {
 }
 
 function curvedLine(x, y) {
-  // if (Math.abs(x[0] - y[0]) < 4 * width) {
-  return [
-    new Vector3(x[0], x[1] - height, x[2]),
-    new Vector3(x[0], y[1], y[2]),
-    new Vector3(y[0] + width, y[1], y[2]),
-  ];
-  // }
+  if (Math.abs(x[0] - y[0]) < 2 * width) {
+    return [
+      new Vector3(x[0], x[1] - height, x[2]),
+      new Vector3(x[0], y[1], y[2]),
+      new Vector3(y[0] + width, y[1], y[2]),
+    ];
+  } else {
+    return [
+      new Vector3(x[0], x[1] + height, x[2]),
+      new Vector3(x[0], y[1], y[2]),
+      new Vector3(y[0] - width, y[1], y[2]),
+    ];
+  }
 }
 
 export { lineMP };
