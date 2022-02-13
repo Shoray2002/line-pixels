a8f4924154fdeae7962afecd97ed09e01f9f5946
Shoray Singhal <lci2020037@iiitl.ac.in>
2022-02-13 14:54:56 +0530

    linear done

--- src/lineMP.mjs
+++ src/lineMP.mjs
@@ -11,59 +11,101 @@ function lineMP(plane1, plane2, type = "linear") {
 }
 
 function straightLine(plane1, plane2) {
+  var vec = [];
   if (plane1[1] == plane2[1]) {
+    //same y value
     if (plane1[0] < plane2[0]) {
-      return [
+      vec = [
         new Vector3(plane1[0] + width, plane1[1], plane1[2]),
         new Vector3(plane2[0] - width, plane2[1], plane2[2]),
       ];
     } else if (plane1[0] > plane2[0]) {
-      return [
+      vec = [
         new Vector3(plane1[0] - width, plane1[1], plane1[2]),
         new Vector3(plane2[0] + width, plane2[1], plane2[2]),
       ];
+    } else {
+      vec = [
+        new Vector3(plane1[0] - width, plane1[1], plane1[2]),
+        new Vector3(plane1[0] - width, plane1[1], plane1[2]),
+      ];
     }
   } else if (plane1[0] == plane2[0]) {
+    //same x value
     if (plane1[1] < plane2[1]) {
-      return [
+      vec = [
         new Vector3(plane1[0], plane1[1] + height, plane1[2]),
         new Vector3(plane2[0], plane2[1] - height, plane2[2]),
       ];
     } else if (plane1[1] > plane2[1]) {
-      return [
+      vec = [
         new Vector3(plane1[0], plane1[1] - height, plane1[2]),
         new Vector3(plane2[0], plane2[1] + height, plane2[2]),
       ];
+    } else {
+      vec = [
+        new Vector3(plane1[0] - height, plane1[1], plane1[2]),
+        new Vector3(plane1[0] - height, plane1[1], plane1[2]),
+      ];
     }
   } else {
+    // vec= [
+    //   new Vector3(plane1[0], plane1[1] - height, plane1[2]),
+    //   new Vector3(plane2[0], plane2[1] + height, plane2[2]),
+    // ];
+
+    // if (plane2[0] > plane1[0] && plane2[1] < plane1[1]) {
+    //   vec= [
+    //     new Vector3(plane1[0], plane1[1] - height, plane1[2]),
+    //     new Vector3(plane2[0], plane2[1] + height, plane2[2]),
+    //   ];
+    // }
+    // if (plane2[0] < plane1[0] && plane2[1] < plane1[1]) {
+    //   vec= [
+    //     new Vector3(plane1[0], plane1[1] - height, plane1[2]),
+    //     new Vector3(plane2[0], plane2[1] + height, plane2[2]),
+    //   ];
+    // }
     if (plane1[1] > plane2[1]) {
       let diff = plane1[1] - plane2[1];
       if (diff < height * 2) {
-        return [
+        vec = [
           new Vector3(plane1[0] - width, plane1[1], plane1[2]),
           new Vector3(plane2[0] + width, plane2[1], plane2[2]),
         ];
       } else {
-        return [
+        vec = [
           new Vector3(plane1[0], plane1[1] - height, plane1[2]),
           new Vector3(plane2[0], plane2[1] + height, plane2[2]),
         ];
       }
     } else if (plane1[1] < plane2[1]) {
       let diff = plane2[1] - plane1[1];
-      if (diff < height * 2) {
-        return [
+      if (diff < height * 2 && plane1[0] > plane2[0]) {
+        vec = [
           new Vector3(plane1[0] - width, plane1[1], plane1[2]),
           new Vector3(plane2[0] + width, plane2[1], plane2[2]),
         ];
+      } else if (diff < height * 2 && plane1[0] < plane2[0]) {
+        vec = [
+          new Vector3(plane1[0] + width, plane1[1], plane1[2]),
+          new Vector3(plane2[0] - width, plane2[1], plane2[2]),
+        ];
       } else {
-        return [
+        vec = [
           new Vector3(plane1[0], plane1[1] + height, plane1[2]),
           new Vector3(plane2[0], plane2[1] - height, plane2[2]),
         ];
       }
     }
+    if (plane1[0] > plane2[0]) {
+      vec = [
+        new Vector3(plane1[0] - width, plane1[1], plane1[2]),
+        new Vector3(plane2[0] + width, plane2[1], plane2[2]),
+      ];
+    }
   }
+  return vec;
 }
 
 function curvedLine(x, y) {
